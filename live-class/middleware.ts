01c428166d7ba0bfd437132b618fe116af5455a9
SAR0406 <sarthak040624@gmail.com>
2025-12-11 14:31:28 +0530

    Replace authMiddleware with clerkMiddleware

--- live-class/middleware.ts
+++ live-class/middleware.ts
@@ -1,20 +1,21 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/sign-in(.*)",
-    "/sign-up(.*)",
-    "/api/webhooks(.*)",
-  ],
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
   matcher: [
-    // Skip static files
     "/((?!_next|.*\\..*).*)",
-
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
